1297d12d70663b4887dc81c2f028b44295aee366
HarmanuAulakh <98173051+HarmanuAulakh@users.noreply.github.com>
2022-02-04 06:41:00 -0500

    added comments

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,4 +1,8 @@
 /*
+Author Harmandeep Singh Aulakh
+Date 4 feb 2022
+*/
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
